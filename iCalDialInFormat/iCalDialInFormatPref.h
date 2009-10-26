b68bd1c3425e531fd40ca5b7f4af17875c700221
igb <igb@phosphorus.mygazoo.com>
2009-10-25 19:24:07 -0700

    initial commit

---
+++ iCalDialInFormat/iCalDialInFormatPref.h
@@ -0,0 +1,19 @@
+//
+//  iCalDialInFormatPref.h
+//  iCalDialInFormat
+//
+//  Created by Ian Brown on 10/25/09.
+//  Copyright (c) 2009 __MyCompanyName__. All rights reserved.
+//
+
+#import <PreferencePanes/PreferencePanes.h>
+
+
+@interface iCalDialInFormatPref : NSPreferencePane 
+{
+
+}
+
+- (void) mainViewDidLoad;
+
+@end
---
+++ iCalDialInFormat/iCalDialInFormatPref.m
@@ -0,0 +1,18 @@
+//
+//  iCalDialInFormatPref.m
+//  iCalDialInFormat
+//
+//  Created by Ian Brown on 10/25/09.
+//  Copyright (c) 2009 __MyCompanyName__. All rights reserved.
+//
+
+#import "iCalDialInFormatPref.h"
+
+
+@implementation iCalDialInFormatPref
+
+- (void) mainViewDidLoad
+{
+}
+
+@end
