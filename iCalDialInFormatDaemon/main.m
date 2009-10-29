d6d09f8feac43a63d1c49f75ddf11aa9582f5814
igb <igb@phosphorus.hccp.org>
2009-10-28 23:59:46 -0700

    working wiht ical now

--- iCalDialInFormatDaemon/main.m
+++ iCalDialInFormatDaemon/main.m
@@ -1,5 +1,7 @@
 #include <CoreFoundation/CoreFoundation.h>
 #include <Foundation/Foundation.h>
+#include <CalendarStore/CalendarStore.h>
+
 
 #import "../iCalDialInFormat/constants.h"
 
@@ -14,10 +16,30 @@ int main (int argc, const char * argv[]) {
 	iPhoneCheckboxValue = CFPreferencesCopyAppValue(FORMAT_FOR_IPHONE,  APP_ID );
 	blackberryCheckboxValue = CFPreferencesCopyAppValue(FORMAT_FOR_BLACKBERRY,  APP_ID );
 	isActiveValue = CFPreferencesCopyAppValue(IS_ACTIVE,  APP_ID );
+	if (CFBooleanGetValue(isActiveValue)) {
 
+		NSLog(@"Formatter is active: %d", CFBooleanGetValue(isActiveValue));
+	
+		NSAutoreleasePool	 *autoreleasepool = [[NSAutoreleasePool alloc] init];
+		// do thread work
+		// Create a predicate to fetch all events for this year
+		NSInteger year = [[NSCalendarDate date] yearOfCommonEra];
+		NSDate *startDate = [[NSCalendarDate dateWithYear:year month:1 day:1 hour:0 minute:0 second:0 timeZone:nil] retain];
+		NSDate *endDate = [[NSCalendarDate dateWithYear:year month:12 day:31 hour:23 minute:59 second:59 timeZone:nil] retain];
+		NSPredicate *eventsForThisYear = [CalCalendarStore eventPredicateWithStartDate:startDate endDate:endDate
+																		 calendars:[[CalCalendarStore defaultCalendarStore] calendars]];
+	
+		// Fetch all events for this year
+		NSArray *events = [[CalCalendarStore defaultCalendarStore] eventsWithPredicate:eventsForThisYear];
+		
+		for (id event in events) {
+			NSLog(@"%@", [event notes]);
+				}
+		
+		
+		[autoreleasepool release];
 
-    NSLog(@"Formatter is active: %d", CFBooleanGetValue(isActiveValue));
-
+	}
 
 
     return 0;
