722d00cfce59f6ff15a93e3bf31ed9c9c297e636
igb <igb@phosphorus.hccp.org>
2009-11-08 18:57:01 -0800

    adding web infrastructure

---
+++ webapp/src/bash/provision.sh
@@ -0,0 +1,5 @@
+scp -i ~/Downloads/click2call.pem ~/Documents/phoneformat/webapp/src/html/index.html root@ec2-75-101-244-132.compute-1.amazonaws.com:/var/www/html/
+
+scp -i ~/Downloads/click2call.pem ~/Documents/phoneformat/webapp/src/html/tel.html root@ec2-75-101-244-132.compute-1.amazonaws.com:/var/www/html/
+
+scp -i ~/Downloads/click2call.pem ~/Documents/phoneformat/webapp/src/img/cal.png root@ec2-75-101-244-132.compute-1.amazonaws.com:/var/www/html/
